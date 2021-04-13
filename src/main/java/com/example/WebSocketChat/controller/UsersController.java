53edd763c3de00e6aee561faf8538558c962e0e7
EugeniySichev <sichev91@inbox.ru>
2021-04-13 23:01:28 +0300

    дописал контроллер, добавил немного фронтэнда, добавил две новые ветки

---
+++ src/main/java/com/example/WebSocketChat/controller/UsersController.java
@@ -0,0 +1,30 @@
+package com.example.WebSocketChat.controller;
+
+import com.example.WebSocketChat.storage.UserStorage;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
+import java.util.Set;
+
+@RestController
+public class UsersController {
+
+    @GetMapping("/registration/{userName}")
+    public ResponseEntity<Void> registration(@PathVariable String userName) {
+        try {
+            System.out.println("handling registraton user request: " + userName);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/fetchAllUsers")
+    public Set<String> fetchAll(){
+        return UserStorage.getInstance().getUsers();
+    }
+}
